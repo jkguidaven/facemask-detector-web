a5cacc0bfa07957887620388a135989b5fa8afde
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2020-05-30 21:19:47 +0800

    Project initialization with gulp script for building app

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # facemask-detector-web
-A web application that demonstrate face mask detection functionality using OpenCV library for javascript. Based on Haar cascade to detect facial features such as nose eyes and mouth. 
+
+A web application that demonstrate face mask detection functionality using OpenCV library for javascript. Based on Haar cascade to detect facial features such as nose eyes and mouth.
+
+## Prerequisites
+
+- [Node](https://nodejs.org/en/) (at least the latest LTS)
+- [Yarn](https://yarnpkg.com/lang/en/docs/install/) (at least 1.0)
+
+## To build and start app
+
+```
+// 1. Clone Repository
+git clone git@github.com:jkguidaven/facemask-detector-web.git
+
+// 2. cd to project directory
+cd ball-motion-web
+
+// 3. Install dependencies
+yarn install
+
+// 4. Build and run app
+yarn start
+```
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const del = require("del");
+const gulp = require("gulp");
+
+const browserify = require("browserify");
+const tsify = require("tsify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const uglify = require("gulp-uglify");
+const sourcemaps = require("gulp-sourcemaps");
+
+const outputDirectory = "dist";
+const sourceDirectory = "src";
+
+gulp.task("clean", function () {
+  return del(`${outputDirectory}/**`, { force: true });
+});
+
+gulp.task("build:html", () => {
+  return gulp.src(`index.html`).pipe(gulp.dest(outputDirectory));
+});
+gulp.task("build:js", () => {
+  return browserify({
+    basedir: ".",
+    debug: true,
+    entries: ["src/main.ts"],
+    cache: {},
+    packageCache: {},
+  })
+    .plugin(tsify)
+    .transform("babelify", {
+      presets: ["es2015"],
+      extensions: [".ts"],
+    })
+    .bundle()
+    .pipe(source("bundle.js"))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(outputDirectory));
+});
+
+gulp.task("build", gulp.series(["build:html", "build:js"]));
+
+gulp.task("watch", function () {
+  return gulp.watch(`**`, gulp.series(["build"]));
+});
